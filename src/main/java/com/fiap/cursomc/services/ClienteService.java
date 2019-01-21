d3a78fe30433b659bdf9c1a8bf15d5e8f17c634c
Marcelo Teixeira <celocesar30@hotmail.com>
2019-01-20 22:42:49 -0200

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/fiap/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.fiap.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fiap.cursomc.domain.Cliente;
+import com.fiap.cursomc.repositories.ClienteRepository;
+import com.fiap.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
