99db173557c035a3a38b815e094e7a190dbbf1b1
Marcelo Teixeira <celocesar30@hotmail.com>
2019-02-05 23:37:48 -0200

    ItemPedido e ItemPedidoPK

--- src/main/java/com/fiap/cursomc/CursomcApplication.java
+++ src/main/java/com/fiap/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.fiap.cursomc.domain.Cidade;
 import com.fiap.cursomc.domain.Cliente;
 import com.fiap.cursomc.domain.Endereco;
 import com.fiap.cursomc.domain.Estado;
+import com.fiap.cursomc.domain.ItemPedido;
 import com.fiap.cursomc.domain.Pagamento;
 import com.fiap.cursomc.domain.PagamentoComBoleto;
 import com.fiap.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.fiap.cursomc.repositories.CidadeRepository;
 import com.fiap.cursomc.repositories.ClienteRepository;
 import com.fiap.cursomc.repositories.EnderecoRepository;
 import com.fiap.cursomc.repositories.EstadoRepository;
+import com.fiap.cursomc.repositories.ItemPedidoRepository;
 import com.fiap.cursomc.repositories.PagamentoRepository;
 import com.fiap.cursomc.repositories.PedidoRepository;
 import com.fiap.cursomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -121,5 +126,18 @@ public class CursomcApplication implements CommandLineRunner {
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+
 	}
 }
--- src/main/java/com/fiap/cursomc/domain/Produto.java
+++ src/main/java/com/fiap/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.fiap.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -19,11 +22,23 @@ public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private Double preco;
+
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
 
 	@JsonBackReference
 	@ManyToMany
@@ -39,31 +54,6 @@ public class Produto implements Serializable {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Produto other = (Produto) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}
 
 	public Integer getId() {
@@ -96,6 +86,39 @@ public class Produto implements Serializable {
 
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItems(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 }
